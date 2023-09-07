8f0677e8d663fbae1d3c06078b0dd204190a2651
Jahongir <dodajoy0777@gmail.com>
2023-09-07 07:22:59 +0500

    save

--- polls/views.py
+++ polls/views.py
@@ -4,17 +4,25 @@ from django.shortcuts import render
 from .models import Oquv_markaz, oqituvchi
 from django.http import JsonResponse
 from .serializer import OquvmarkazSerializer, oqituvchiSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-def all(request):
-    jami=Oquv_markaz.objects.all()
-    savelist = OquvmarkazSerializer(jami, many=True)
-    # for i in jami:
-    #     savelist.append({
-    #         'name':i.name,
-    #         'numbers of room':i.xonalar_soni
-    #     })
-    return JsonResponse(savelist.data, safe=False)
+# def all(request):
+#     jami=Oquv_markaz.objects.all()
+#     savelist = OquvmarkazSerializer(jami, many=True)
+#     # for i in jami:
+#     #     savelist.append({
+#     #         'name':i.name,
+#     #         'numbers of room':i.xonalar_soni
+#     #     })
+#     return JsonResponse(savelist.data, safe=False)
+
+class getOquvMarkaz(APIView):
+    def get(self, request):
+        bor = Oquv_markaz.objects.all()
+        srr2=OquvmarkazSerializer(bor, many=True)
+        return Response(srr2.data)
     
     
 
@@ -25,3 +33,13 @@ def detail(request, whichid):
     # each = oqituvchi.objects.get(id=whichid)
     # data={'O`qituvchi ismi':each.ismi, 'O`qituvchi nomeri':each.tel_nomer}
     return JsonResponse(ss.data, safe=False)
+
+
+class postoquvmarkaz(APIView):
+    def post(self, request):
+        main_body=request.data
+        sss = OquvmarkazSerializer(data=main_body)
+        if sss.is_valid():
+            sss.save()
+            return Response(sss.data)
+        return Response(sss.errors)
