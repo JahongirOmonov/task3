88c8fc7eb6d34e2345a2908b1f83830be59aa023
Jahongir <dodajoy0777@gmail.com>
2023-09-16 07:48:27 +0500

    last

--- polls/views.py
+++ polls/views.py
@@ -6,40 +6,62 @@ from django.http import JsonResponse
 from .serializer import OquvmarkazSerializer, oqituvchiSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 
-# def all(request):
-#     jami=Oquv_markaz.objects.all()
-#     savelist = OquvmarkazSerializer(jami, many=True)
-#     # for i in jami:
-#     #     savelist.append({
-#     #         'name':i.name,
-#     #         'numbers of room':i.xonalar_soni
-#     #     })
-#     return JsonResponse(savelist.data, safe=False)
+class GetAllOquvMarkaz(generics.ListAPIView):
+    queryset=Oquv_markaz.objects.all()
+    serializer_class=OquvmarkazSerializer
 
-class getOquvMarkaz(APIView):
-    def get(self, request):
-        bor = Oquv_markaz.objects.all()
-        srr2=OquvmarkazSerializer(bor, many=True)
-        return Response(srr2.data)
-    
-    
+class GetDetailOquvMarkaz(generics.RetrieveAPIView):
+    queryset = Oquv_markaz.objects.all()
+    serializer_class=OquvmarkazSerializer
+
+class PostOquvMarkaz(generics.CreateAPIView):
+    queryset=Oquv_markaz.objects.all()
+    serializer_class=OquvmarkazSerializer
+
+class PatchOquvMarkaz(generics.UpdateAPIView):
+    queryset=Oquv_markaz.objects.all()
+    serializer_class=OquvmarkazSerializer
+
+class DeleteOquvMarkaz(generics.DestroyAPIView):
+    queryset=Oquv_markaz.objects.all()
+    serializer_class=OquvmarkazSerializer
+
+class PostGetOquvMarkaz(generics.ListCreateAPIView):
+    queryset=Oquv_markaz.objects.all()
+    serializer_class=OquvmarkazSerializer
+
+class AllFunctionOquvMarkaz(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Oquv_markaz.objects.all()
+    serializer_class=OquvmarkazSerializer
 
 
-def detail(request, whichid):
-    each=oqituvchi.objects.filter(id=whichid)
-    ss=oqituvchiSerializer(each, many=True)
-    # each = oqituvchi.objects.get(id=whichid)
-    # data={'O`qituvchi ismi':each.ismi, 'O`qituvchi nomeri':each.tel_nomer}
-    return JsonResponse(ss.data, safe=False)
+class GetAllOqituvchi(generics.ListAPIView):
+    queryset=oqituvchi.objects.all()
+    serializer_class=oqituvchiSerializer
 
+class GetDetailOqituvchi(generics.RetrieveAPIView):
+    queryset = oqituvchi.objects.all()
+    serializer_class=oqituvchiSerializer
 
-class postoquvmarkaz(APIView):
-    def post(self, request):
-        main_body=request.data
-        sss = OquvmarkazSerializer(data=main_body)
-        if sss.is_valid():
-            sss.save()
-            return Response(sss.data)
-        return Response(sss.errors)
+class PostOqituvchi(generics.CreateAPIView):
+    queryset=oqituvchi.objects.all()
+    serializer_class=oqituvchiSerializer
+
+class PatchOqituvchi(generics.UpdateAPIView):
+    queryset=oqituvchi.objects.all()
+    serializer_class=oqituvchiSerializer
+
+class DeleteOqituvchi(generics.DestroyAPIView):
+    queryset=oqituvchi.objects.all()
+    serializer_class=oqituvchiSerializer
+
+class PostGetOqituvchi(generics.ListCreateAPIView):
+    queryset=oqituvchi.objects.all()
+    serializer_class=oqituvchiSerializer
+
+class AllFunctionOqituvchi(generics.RetrieveUpdateDestroyAPIView):
+    queryset=oqituvchi.objects.all()
+    serializer_class=oqituvchiSerializer
